2d6217d13c44905c0f289f20b1f20190c79a6e91
eons44 <eons@eons.dev>
2022-08-30 21:02:08 -0700

    update workflows and build system

--- inc/hello_world.py
+++ inc/hello_world.py
@@ -4,17 +4,17 @@ from eons import UserFunctor
 
 
 # Class name is what is used at cli, so we defy convention here in favor of ease-of-use.
-class hello_world(Builder):
+class hello_world(UserFunctor):
     def __init__(this, name="Hello World"):
         super().__init__(name)
 
         this.clearBuildPath = False
         this.supportedProjectTypes = []
 
-    # Required Builder method. See that class for details.
-    def DidBuildSucceed(this):
+    # Required UserFunctor method. See that class for details.
+    def DidUserFunctionSucceed(this):
         return True 
 
-    # Required Builder method. See that class for details.
-    def Build(this):
-        logging.info("Hello world!")
+    # Required UserFunctor method. See that class for details.
+    def UserFunction(this, **kwargs):
+        print("Hello world!")
